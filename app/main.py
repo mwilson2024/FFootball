3eb7f033be1caca4cf47b32f5f251ae3d6d44030
mwilson2024 <mwilson10@oakland.edu>
2026-08-31 14:19:03 -0400

    Enable player cards in auction room

--- app/main.py
+++ app/main.py
@@ -242,7 +242,7 @@ from scripts.import_auction_csv_as_draft import (
 
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
-templates.env.globals["asset_version"] = "20260831.13"
+templates.env.globals["asset_version"] = "20260831.14"
 SESSION_SIGNING_SECRET = ""
 
 
